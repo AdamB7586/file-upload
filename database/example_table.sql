0676c1074848deedbcd07e24321b391e63e29557
Adam Binnersley <abinnersley@gmail.com>
2017-11-24 15:39:11 +0000

    Initial commit

---
+++ database/example_table.sql
@@ -0,0 +1,13 @@
+--
+-- Table structure for table `uploads`
+--
+
+CREATE TABLE IF NOT EXISTS `uploads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file` varchar(100) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `size` int(11) NOT NULL DEFAULT '0',
+  `content` mediumblob NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`file`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
