f575d55ccdb4d079ced07aa77438b09513c368c5
Adam Binnersley <abinnersley@gmail.com>
2017-11-27 10:14:23 +0000

    Add basic unit test

---
+++ tests/FileUploadDBALTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Upload\Tests;
+
+use DBAL\Database;
+use Upload\FileUploadDB;
+use PHPUnit\Framework\TestCase;
+
+class FileUploadDBTest extends TestCase {
+    
+    protected $upload;
+    
+    public function setUp(){
+        $this->dbc = new Database('localhost', 'username', 'password', 'test_db', false, false, true, 'sqlite');
+        $this->upload = new FileUploadDB();
+        $this->upload->databaseConnect($this->dbc);
+    }
+    
+    public function tearDown(){
+        unset($this->upload);
+    }
+    
+    public function testExample(){
+        $this->markTestIncomplete('This test has not yet been implemented');
+    }
+}
