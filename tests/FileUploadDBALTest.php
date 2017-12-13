bfb682a20b1cd3d1e72d1f6392bdc7fe6ed78064
Adam Binnersley <abinnersley@gmail.com>
2017-12-13 16:37:04 +0000

    Update unit testing files

--- tests/FileUploadDBALTest.php
+++ tests/FileUploadDBALTest.php
@@ -2,17 +2,18 @@
 namespace Upload\Tests;
 
 use DBAL\Database;
-use Upload\FileUploadDB;
+use Upload\FileUploadDBAL;
 use PHPUnit\Framework\TestCase;
 
 class FileUploadDBALTest extends TestCase {
     
+    protected $dbc;
     protected $upload;
     
     public function setUp(){
         $this->dbc = new Database('localhost', 'username', 'password', 'test_db', false, false, true, 'sqlite');
-        $this->upload = new FileUploadDB();
-        $this->upload->databaseConnect($this->dbc);
+        $this->upload = new FileUploadDBAL($this->dbc);
+        //$this->upload->databaseConnect();
     }
     
     public function tearDown(){
--- tests/FileUploadDBTest.php
+++ tests/FileUploadDBTest.php
@@ -2,14 +2,22 @@
 namespace Upload\Tests;
 
 use Upload\FileUploadDB;
+use PDO;
 use PHPUnit\Framework\TestCase;
 
 class FileUploadDBTest extends TestCase {
     
+    protected $dbc;
     protected $upload;
     
     public function setUp(){
-        $this->upload = new FileUploadDB();
+        $this->dbc = new PDO(
+            'sqlite::memory:',
+            null,
+            null,
+            array(PDO::ATTR_PERSISTENT => true)
+        );
+        $this->upload = new FileUploadDB($this->dbc);
     }
     
     public function tearDown(){
